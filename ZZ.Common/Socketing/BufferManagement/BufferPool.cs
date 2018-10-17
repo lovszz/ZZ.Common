247c07e4459417b7f2a59ed0d263f729c9aacbce
Apliu <apliu@foxmail.com>
2018-10-17 16:55:42 +0800

    更正缓冲池设计

--- ZZ.Common/Socketing/BufferManagement/BufferPool.cs
+++ ZZ.Common/Socketing/BufferManagement/BufferPool.cs
@@ -5,27 +5,62 @@ using System.Text;
 
 namespace ZZ.Common.Socketing.BufferManagement
 {
+    /// <summary>
+    /// 缓冲池满的处理方式
+    /// </summary>
+    public enum BufferFullDeal
+    {
+        /// <summary>
+        /// 继续添加
+        /// </summary>
+        CONTINUE = 1,
+        /// <summary>
+        /// 返回失败
+        /// </summary>
+        RETURN = 2,
+        /// <summary>
+        /// 抛出异常
+        /// </summary>
+        THROW = 3
+    }
+
     public class BufferPool
     {
         private ConcurrentStack<byte[]> _store;
+        private Int64 maxCount;
+        private BufferFullDeal bufferFullDeal;
 
-        public BufferPool(int bufferSize,int initialCount)
+        public BufferPool(Int64 initialCount, BufferFullDeal initialFullDeal = BufferFullDeal.CONTINUE)
         {
             _store = new ConcurrentStack<byte[]>();
-            for (int i=0;i< initialCount;i++)
-            {
-                var item = new byte[bufferSize];
-                _store.Push(item);
-            }
+            maxCount = initialCount;
+            bufferFullDeal = initialFullDeal;
         }
-        public bool TryGet(out byte[] result)
+
+        public bool TryPop(out byte[] result)
         {
             return _store.TryPop(out result);
         }
-        public void Free(ref byte[] result)
+
+        public Boolean Push(byte[] result)
         {
-            Array.Clear(result, 0, result.Length);
+            //栈池已满
+            if (_store.Count > maxCount)
+            {
+                switch (bufferFullDeal)
+                {
+                    case BufferFullDeal.CONTINUE:
+                        break;
+                    case BufferFullDeal.RETURN:
+                        return false;
+                    case BufferFullDeal.THROW:
+                        throw new OverflowException("Buffer pool is full");
+                    default:
+                        break;
+                }
+            }
             _store.Push(result);
+            return true;
         }
     }
 }
